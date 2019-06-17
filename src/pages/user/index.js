4ce090be9c1d165d86564d9c152bffdfea4a1f54
ltaoo <litaowork@aliyun.com>
2019-06-17 21:41:46 +0800

    chore: 增加注销按钮

--- src/pages/user/index.js
+++ src/pages/user/index.js
@@ -4,16 +4,26 @@ import {
   Text,
 } from '@tarojs/components';
 import {
+  AtButton,
   AtTabBar,
+  AtAvatar,
+  AtActionSheet,
+  AtActionSheetItem,
 } from 'taro-ui';
 import { observer, inject } from '@tarojs/mobx';
 
 import HeadCard from '../../components/HeadCard';
 import { tabList } from '../../constants';
 
+import './index.less';
+
 @inject('global')
 @observer
 export default class User extends Taro.Component {
+  state = {
+    actionSheetVisible: false,
+  };
+
   componentDidMount() {
     const { path } = this.$router;
     const { global } = this.props;
@@ -25,13 +35,46 @@ export default class User extends Taro.Component {
     global.changeTabBarIndex(value);
   }
 
+  showActionSheet = () => {
+    const { actionSheetVisible } = this.state;
+    this.setState({
+      actionSheetVisible: !actionSheetVisible,
+    });
+  }
+
+  handleActionSheetClosed = () => {
+    this.setState({
+      actionSheetVisible: false,
+    });
+  }
+
+  logout = () => {
+    const { global } = this.props;
+    global.logout();
+  }
+
   render() {
-    const { global: { currentTabBarIndex } } = this.props;
+    const { actionSheetVisible } = this.state;
+    const { global: { user, currentTabBarIndex } } = this.props;
     return (
       <View>
-        <HeadCard title="用户中心" />
+        <HeadCard title="用户中心" desc="修改用户名或者注销" />
         <View className="page__content">
+          <View className="user__avatar-wrapper">
+            <AtAvatar size="large" className="user__avatar user__avatar--large" circle text={user.name}></AtAvatar>
+            <Text className="user__name">{user.name}</Text>
+          </View>
+          <AtButton type="danger" onClick={this.showActionSheet}>注销</AtButton>
         </View>
+        <AtActionSheet
+          isOpened={actionSheetVisible}
+          cancelText="取消"
+          onClose={this.handleActionSheetClosed}
+        >
+          <AtActionSheetItem onClick={this.logout}>
+            <Text style={{ color: 'red' }}>注销</Text>
+          </AtActionSheetItem>
+        </AtActionSheet>
         <AtTabBar
           fixed
           tabList={tabList}
